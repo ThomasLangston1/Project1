6368ba44d4f3c22b9161e860cb5b0e7a5e73b55a
thomaslangston <Thomaslangston10@gmail.com>
2019-11-07 09:04:48 -0600

    Added new feature

--- Index.js
+++ Index.js
@@ -1,2 +1,5 @@
 const buttons = document.querySelectorAll('button');
 const result = document.getElementById('result');
+let newWinner = () => {
+    const winner = buttons[math.floor(math.random() * buttons.length)]
+}
