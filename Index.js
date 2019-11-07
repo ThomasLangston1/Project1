ddfb5add46b0ef577b7d22c02f19cb6ed20692d0
thomaslangston <Thomaslangston10@gmail.com>
2019-11-07 09:10:52 -0600

    Last Feature

--- Index.js
+++ Index.js
@@ -1,5 +1,19 @@
 const buttons = document.querySelectorAll('button');
 const result = document.getElementById('result');
 let newWinner = () => {
-    const winner = buttons[math.floor(math.random() * buttons.length)]
-}
+    const winner = buttons[math.floor(math.random() * buttons.length)];
+
+    return winner.innerHTML;
+};
+
+[].forEach.call(buttons, (button) =>{
+    button.addEventListener('click', (e) =>{
+        const winner = newWinner();
+
+        if(e.target.innerHTML === winner) {
+            result.innerHTML = 'You win! Wanna play again?';
+        } else {
+            result.innerHTML = 'You lose! Wanna play again?';
+        }
+    })
+})
